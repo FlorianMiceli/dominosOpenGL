4163c61513a6b3b5d1e48bac4ccd65ac1357c55b
Tourence <hugo.pelegue@gmail.com>
2024-06-06 12:46:20 +0200

    coef correction

--- include/physics.h
+++ include/physics.h
@@ -5,7 +5,7 @@
 
 void gravity(double delta_t, Animation &anim)
 {
-    double k = 0.5;
+    double k = 0.1;
     anim.setAccel(Vector(-k * anim.getSpeed().x, -9.81, -k * anim.getSpeed().z));
     anim.setSpeed(Vector(anim.getSpeed().x + delta_t * anim.getAccel().x,
                          anim.getSpeed().y + delta_t * anim.getAccel().y,
@@ -19,7 +19,7 @@ void solid(Animation &anim)
 {
     if (anim.getPos().y < 0.0)
     {
-        anim.setSpeed(Vector(anim.getSpeed().x, -anim.getSpeed().y * 0.3, anim.getSpeed().z));
+        anim.setSpeed(Vector(anim.getSpeed().x*0.95, -anim.getSpeed().y * 0.35, anim.getSpeed().z*0.95));
         anim.setPos(Point(anim.getPos().x, -anim.getPos().y * 0.3, anim.getPos().z));
     }
 }
